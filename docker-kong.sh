38eaac7321da165ee2fbaa1de1c16da7ad2b9724
kaamwallay <thekaamwallay@gmail.com>
2022-07-12 16:22:34 +0500

    Initial commit with config files and CI CD setup

--- docker-kong.sh
+++ docker-kong.sh
@@ -1,5 +1,5 @@
-#!/bin/bash
-# this file goes in the EC@ server (circleci user) home directory
+!/bin/bash
+
 set -e
 
 DOCKER_IMAGE=$1
@@ -14,7 +14,7 @@ if [[ $# -lt 4 ]] ; then
         echo 'Deployment process is exiting...'
         exit 1
 fi
-echo DOCKER_IMAGE
+echo $DOCKER_IMAGE
 
 # Check for running container & stop it before starting a new one
 # echo "Checking if container name "
@@ -33,23 +33,20 @@ echo "Starting Docker image name: $DOCKER_IMAGE"
 echo $DOCKER_PWD | sudo docker login -u $DOCKER_LOGIN --password-stdin
 
 # sudo docker run -d -p 8000:8000 --restart always --name $CONTAINER_NAME $DOCKER_IMAGE
-
-# sudo docker ps -a
-
-
-sudo docker run -d -p 4000:4000 --restart always --name $CONTAINER_NAME $DOCKER_IMAGE
-
 sudo docker ps -a
 
+echo "\n"
+echo $CONTAINER_NAME
+echo $DOCKER_IMAGE
 
 sudo docker run -d --network=host --restart always \
-    -e "KONG_DATABASE=off" \
-    -e "KONG_DECLARATIVE_CONFIG=/app/kong.yml" \
-    -e "KONG_PROXY_ACCESS_LOG=/dev/stdout" \
-    -e "KONG_ADMIN_ACCESS_LOG=/dev/stdout" \
-    -e "KONG_PROXY_ERROE_LOG=/dev/stderr" \
-    -e "KONG_ADMIN_ERROR_LOG=/dev/stderr" \
-    -e "KONG_ADMIN_LISTEN=0.0.0.0:8001, 0.0.0.0:8444 ssl" \
-    --name $CONTAINER_NAME $DOCKER_IMAGE
+-e "KONG_DATABASE=off" \
+-e "KONG_DECLARATIVE_CONFIG=/app/kong.yml" \
+-e "KONG_PROXY_ACCESS_LOG=/dev/stdout" \
+-e "KONG_ADMIN_ACCESS_LOG=/dev/stdout" \
+-e "KONG_PROXY_ERROE_LOG=/dev/stderr" \
+-e "KONG_ADMIN_ERROR_LOG=/dev/stderr" \
+-e "KONG_ADMIN_LISTEN=0.0.0.0:8001, 0.0.0.0:8444 ssl" \
+--name $CONTAINER_NAME $DOCKER_IMAGE
 
 sudo docker ps -a
